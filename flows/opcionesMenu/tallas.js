ef934b4b233dc93087397e2af817e04055d51a25
jdiazo3 <106680414+jdiazo3@users.noreply.github.com>
2025-02-28 14:13:04 -0500

    Update tallas.js

--- flows/opcionesMenu/tallas.js
+++ flows/opcionesMenu/tallas.js
@@ -2,8 +2,11 @@ const { addKeyword, addFlow, EVENTS } = require('@bot-whatsapp/bot');
 const messages = require('../enums/mensajes.js');
 const path = require('path');
 const flowMenu = require('../menu.js');
-const pdfPath = path.resolve(__dirname, '../../docs/talla1.jpg'); 
-const pdfPath1 = path.resolve(__dirname, '../../docs/talla2.jpg'); 
+const DOMAIN = process.env.DOMAIN || 'localhost:3000';
+const isLocalhost = DOMAIN.includes('localhost');
+const pdfPath = `${isLocalhost ? 'http' : 'https'}://${DOMAIN}/docs/talla1.jpg`;
+const pdfPath1 =`${isLocalhost ? 'http' : 'https'}://${DOMAIN}/docs/talla2.jpg`; // Asegúrate de que el archivo esté aquí
+
 const { v4: uuidv4 } = require('uuid');
 const ramdomString = uuidv4();
 
@@ -16,7 +19,7 @@ const flowTallas = addKeyword(ramdomString)
         media: pdfPath1
     }).addAnswer(messages.furtherAssistance, { capture: true }, async (ctx, { gotoFlow,fallBack, flowDynamic }) => {
             if (!['si', 'no'].includes(ctx.body.toLowerCase())) {
-                        return fallBack(messages.colorFallback);
+                        return fallBack(messages.sinoFallback);
                     }
                     console.log('antes del si');
                     if('si'===ctx.body.toLowerCase()){
